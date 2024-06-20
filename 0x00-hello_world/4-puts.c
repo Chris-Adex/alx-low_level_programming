dddb86b87433262f394ca3b76d92cd54e0fd2a88
Oladipo Adediran <adediranchristopher@gmail.com>
2024-06-20 17:01:14 +0100

    prints using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - print strings
+* Return: always zero (success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
