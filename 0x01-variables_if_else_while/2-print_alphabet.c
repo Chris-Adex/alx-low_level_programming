bab2a856414c04626c3d3a8f7074d23aa21d0adb
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-22 00:50:18 +0100

    print alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0(success)
+*/
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
