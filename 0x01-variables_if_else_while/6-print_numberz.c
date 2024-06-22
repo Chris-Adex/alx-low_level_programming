4db061dadda4612f428346c2e5e45be9e3551646
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-22 02:10:15 +0100

    print integer using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,16 @@
 
 /**
  * main - prints the single digits numbers in base 10 starting from 0
- * Return: 0(success)
+ * using putchar only and int variable
+ * * Return: 0(success)
 */
 
 int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9;)
-		printf("%d", n++);
-	printf("\n");
+	for (n = 0; n <= 9; n++)
+		putchar(n + '0');
+	putchar('\n');
 	return (0);
 }
