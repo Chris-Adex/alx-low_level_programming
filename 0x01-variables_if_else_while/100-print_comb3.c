793adccbfdc0e617c42ddca5a1894dc622968e29
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-24 21:52:59 +0100

    all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = 0; a <= 8; a++)
+{
+	for (b = a + 1; b <= 9; b++)
+	{
+	putchar(a + '0');
+	putchar(b + '0');
+
+	if (a <= 7)
+	putchar(',');
+	putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
+
