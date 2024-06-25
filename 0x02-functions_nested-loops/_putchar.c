ba2724c2c9df722d21d1c3ede9b08e78eb94a709
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-25 12:30:23 +0100

    print _putchar

---
+++ 0x02-functions_nested-loops/_putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int _putchar (char c);
+
+
+int main(void)
+{
+char  = "_putchar";
+_putchar(str);
+_putchar('\n');
+return (0);
+}
+
+
+int _putchar (char c)
+{
+return (write(1, &c, 1));
+}
