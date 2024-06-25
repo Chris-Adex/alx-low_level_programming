da4969969453bf6ffc38e876aaecbe007cd27452
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-25 05:58:29 +0100

    combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+/**
+ * main - entry point
+ * Return: Always return (0)
+ */
+
+int main(void)
+{
+int a;
+int b;
+
+for (a = 0; a <= 98; a++)
+{
+	for (b = a + 1; b <= 99; b++)
+	{
+		putchar((a / 10) + '0');
+		putchar(a + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar(b + '0');
+
+		if (a <= 98)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
