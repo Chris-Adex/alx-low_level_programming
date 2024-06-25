525616099d118e5ba547aa2c15193085cc5f5ca7
Oladipo Adediran <adediranchristopher@gmail.com>
2024-06-25 18:14:36 +0100

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "ade.h"
+
+/**
+*print_sign - checks whether n > 0, n < 0 or n == 0
+*@n: input parameter
+*
+*Return: returns 1 if n > 0, 0 if n == 0 and -1 if n < 0
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
