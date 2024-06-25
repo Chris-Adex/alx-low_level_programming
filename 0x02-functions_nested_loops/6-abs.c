8b4a6ce90d8689a629c01e1ce481a593eaa64453
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-25 18:59:01 +0100

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_abs - computes absolute value of an integer
+*@n: any integer value
+*Return: returns absolute value of n
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
