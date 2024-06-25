eafc712431e2a38729da1067ba376d51451e626f
Oladipo Adediran <adediranchristopher@gmail.com>
2024-06-25 16:58:05 +0100

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "ade.h"
+
+/**
+*_islower - checks for lowercase letters
+*@c: int data type
+*
+*Return: returns 1 if positive and 0 if otherwise
+*/
+
+int _islower(int c)
+{
+char a;
+c = c + '0';
+
+for (a = 'a'; a <= 'z'; a++)
+{
+if (c == a)
+	return (1);
+else
+return (0);
+}
+}
