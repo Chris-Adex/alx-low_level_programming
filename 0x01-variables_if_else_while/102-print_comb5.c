90aa276ac8bd509bc549a834890c7fbf26c4af92
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-25 06:40:29 +0100

    correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,10 +15,10 @@ for (a = 0; a <= 98; a++)
 	for (b = a + 1; b <= 99; b++)
 	{
 		putchar((a / 10) + '0');
-		putchar(a + '0');
+		putchar((a % 10) + '0');
 		putchar(' ');
 		putchar((b / 10) + '0');
-		putchar(b + '0');
+		putchar((b % 10) + '0');
 
 		if (a <= 98)
 		{
