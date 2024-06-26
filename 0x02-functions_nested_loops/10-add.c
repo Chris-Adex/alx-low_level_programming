3c5233662dd5efe7b640a9afdf0ea03904081ab7
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-26 02:50:22 +0100

    add two integeres

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - adds tow integers
+* @x: first integer value
+* @y: second integer value
+* Return: returns the end result of the addition
+*/
+
+int add(int x, int y)
+{
+_putchar((x + y) + '0');
+return ((x + y));
+}
