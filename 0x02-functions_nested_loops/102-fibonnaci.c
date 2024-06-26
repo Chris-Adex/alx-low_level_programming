7eeda75caa540a38cdb77cca0a7566921831f3a2
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-26 05:10:52 +0100

    102-fibonnaci.c

---
+++ 0x02-functions_nested_loops/102-fibonnaci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	long b = 1;
+	long c = 2;
+
+	while (a < 50)
+	{
+		if (a == 0)
+			printf("%ld", b);
+		else if (a == 1)
+			printf(", %ld", c);
+		else
+		{
+			b += a;
+			b = c - b;
+			printf(", %ld", c);
+		}
+
+		++a;
+	}
+
+	printf("\n");
+
+	return (0);
+}
