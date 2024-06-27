f33ae3f65431fa49531a94aada79838a5f069551
Oladipo Adediran <adediranchristopher@gmail.com>
2024-06-27 11:29:47 +0100

    print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ if ((year % 4 == 0 || year % 400 == 0) && (year % 100 != 0))
 {
 	if ((month > 2 && day > 60) || (month == 2 && day == 60))
 	{
-	day++
+	day++;
 	}
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
