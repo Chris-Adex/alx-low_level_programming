f23694d1d514f791a04b511ef9a0aced78a4909f
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-28 06:44:36 +0100

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,13 +16,11 @@ while (n <= 9)
 {
 for (x = 0; x <= 14; x++)
 {
-if (x > 9)
-{
-_putchar(x / 10);
-_putchar(x % 10);
+	if (x > 9)
+	_putchar((x / 10) + '0');
+	_putchar((x % 10)+ '0');
 }
 _putchar('\n');
 n++;
-}
 }
 }
