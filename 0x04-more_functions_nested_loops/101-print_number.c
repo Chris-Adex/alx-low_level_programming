c7e017c00f5af3839e5527955556113e1a6597a3
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-28 22:14:20 +0100

    print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - prints integer with _putchar
+ * @n: takes number
+ *
+ */
+
+void print_number(int n)
+{
+unsigned int x;
+
+if (n < 0)
+{
+_putchar('-');
+x = -n;
+}
+else
+{
+x = n;
+}
+if (m / 10 != 0)
+print_number(x / 10);
+_putchar((x % 10) + '0');
+}
