52ccc2007b9c0f34ee4c26168e363ff38fd9be9a
Oladipo Adediran <adediranchristopher@gmail.com>
2024-06-28 05:59:57 +0100

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 101; x++)
+	{
+		if (x % 3 == 0 && x % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (x % 5 == 0 && x % 3 != 0)
+			printf(" Buzz");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf(" FizzBuzz");
+		else if (x == 1)
+			printf("%d", x);
+		else
+			printf(" %d", x);
+	}
+	printf("\n");
+	return (0);
+}
