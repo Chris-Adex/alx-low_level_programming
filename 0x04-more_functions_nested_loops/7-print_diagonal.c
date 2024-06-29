d54d598fd32b204c7b11d9c64c797e4b298e56e3
Oladipo Adediran <adediranchristopher@gmail.com>
2024-06-29 04:08:31 +0100

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,13 +7,16 @@
 
 void print_diagonal(int n)
 {
-int x;
+int x, y;
 
 if (n > 0)
 {
-	for (x = 1; x <= n; x++)
+	for (x = 0; x <= n; x++)
+	{
+	for (y = 0; y < x: y++)
 	{
 		_putchar(' ');
+	}
 		_putchar('\\');
 	}
 	_putchar('\n');
