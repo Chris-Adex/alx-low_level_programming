a5e0681d9fad9385c39ccd5128c26df6b34f7dde
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-29 03:34:53 +0100

    prime fctor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+long x, num;
+
+num = 612852475143;
+for (x = 2; x < num; x++)
+{
+while (num % i == 0)
+num = num / i;
+}
+
+printf("%lu\n", num);
+
+return (0);
+}
+
+
