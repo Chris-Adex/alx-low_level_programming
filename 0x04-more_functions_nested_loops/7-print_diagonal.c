e6c67bb5b4fc9ef798a18c6e632f2a385ff40e0c
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-06-29 03:58:23 +0100

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,22 +3,21 @@
 /**
  * print_diagonal - draws diagonal line
  * @n: function parameter
- *
  */
 
 void print_diagonal(int n)
 {
 int x;
 
-	if (n != 0 || n > 0)
+if (n != 0 || n > 0)
+{
+	for (x = 1; x <= n; x++)
 	{
-		for (x = 1; x <= n; x++)
-		{
-			_putchar(' ');
-			_putchar('\\');
-		}
-		_putchar('\n');
+		_putchar(' ');
+		_putchar('\\');
 	}
-	else
-		_putchar('\n');
+	_putchar('\n');
+}
+else
+_putchar('\n');
 }
