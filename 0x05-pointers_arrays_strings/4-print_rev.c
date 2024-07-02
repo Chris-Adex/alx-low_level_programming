79571e64c5fb098ce15cf03a2eda57ead53f9bbd
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-07-02 03:09:41 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s:string to be reversed
+ *
+ */
+
+void print_rev(char *s)
+{
+int i;
+int n;
+
+i = 0
+
+for (n = 0; s[n] != '\0'; n++)
+{
+}
+for (i = n - 1; i >=: 0; i--)
+{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
