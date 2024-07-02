6d5597962fa68f85f63507c920a999541ea33bd1
Oladipo Adediran <adediranchristopher@gmail.com>
2024-07-02 02:06:03 +0100

    length counting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - function that returns the lenght of a string
+* @s: string of words
+*
+* Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+int len;
+
+len = strlen(s);
+return (len);
+}
