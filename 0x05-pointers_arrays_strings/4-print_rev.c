f1dc6072870cfa52e0f8b6a658046fd6e1c842b5
Oladipo Adediran <adediranchristopher@gmail.com>
2024-07-02 03:14:58 +0100

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@ void print_rev(char *s)
 int i;
 int n;
 
-i = 0
+i = 0;
 
-for (n = 0; s[n] != '\0'; n++)
+for (n = 0; s[n] != 0; n++)
 {
 }
-for (i = n - 1; i >=: 0; i--)
+for (i = n - 1; i >= 0; i--)
 {
-		_putchar(s[i]);
-	}
-	_putchar('\n');
+_putchar(s[i]);
+}
+_putchar('\n');
 }
