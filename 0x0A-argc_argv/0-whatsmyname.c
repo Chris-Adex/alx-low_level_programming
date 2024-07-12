a4e1e8ce02491a7b673cb97df888eb1833bbb06a
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-07-12 23:39:50 +0100

    initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - funtion to print name of program
+ *
+ * @argc: argument count
+ * @argv: array of cmd line argument
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
