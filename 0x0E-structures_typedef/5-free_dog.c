187f6a2611e0b9fe52dcade70bc62f44b9fdec96
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-07-23 05:28:59 +0100

    task 6

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - function that frees dogs
+  * @d: pointer to struct dog
+  *
+  *Return: void (no return value)
+  */
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+}
+
+free(d->name);
+free(d->owner);
+free(d);
+
+}
+
