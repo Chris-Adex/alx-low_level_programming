badd555a6fa808f98568baed5cf430d62d85333e
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-07-24 21:09:56 +0100

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+
+/**
+ * print_name - prints a name
+ * @name: pointer to name to be printed
+ * @f: pointer to a function that prints name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+		f(name);
+}
