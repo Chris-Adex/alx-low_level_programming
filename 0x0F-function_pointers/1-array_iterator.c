f937e4137c1fd4ab0f9116f64f69b69f1ef54c28
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-07-24 21:39:07 +0100

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+
+/**
+  * array_iterator - funct that executes a func given as parameter
+  * @array: array of elements
+  * @size: size of array
+  * @action: pointer to function
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	if (array && action)
+	{
+		while (size-- > 0)
+		{
+			action(*array);
+			array++;
+		}
+	}
+}
