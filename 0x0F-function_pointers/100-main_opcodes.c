83c5189aa8a78996281c07b6c3a5baac527a102c
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-07-25 03:11:12 +0100

    task 5

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - print opcodes of main function
+  *@argc: number of arguments
+  *@argv: array of pointers
+  *
+  *Return: 0 (sucess)
+  */
+
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	
+	i = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while (i < bytes)
+	{
+		printf("%.2x", *((char *)main + i));
+		i++;
+		if (i  < (bytes - 1))
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
