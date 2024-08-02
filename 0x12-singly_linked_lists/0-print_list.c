0b11bcc00cde0fe63d4039103c725d8ac47fcbdf
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-08-02 03:27:30 +0100

    initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: singly linked list_t list
+ * 
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node;
+	nodes = 0;
+	
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	nodes++;
+	}
+	return (nodes);
+} 
