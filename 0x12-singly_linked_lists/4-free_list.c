254ef130a29f68929bf42fac3eb1180ac81d331f
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-08-02 04:50:18 +0100

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that fress a list_t list
+ * @head: head of the list_t list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *node;
+
+	while (head != NULL)
+	{
+		node = head->next;
+		free(node->str);
+		free(node);
+		head = node;
+	}
+}
