f275e68071184a2caf12392c41b917a87fb77c20
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-08-02 04:37:58 +0100

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,6 +18,11 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
+	if (strdup(str) == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	for (i = 0; str[i];)
 		i++;
