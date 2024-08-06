a76ec488bdebfcab722b30b011f6c8fba0460a49
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-08-06 20:51:04 +0100

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a listint_t list
+ * @head: head address
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+}
