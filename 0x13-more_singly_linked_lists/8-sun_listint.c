fe2efe5a3a2547fb4fc5ed95a44e4654029c84ee
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-08-07 04:05:20 +0100

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sun_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sums of all the data (n) of a listint_t linked list
+ * @head : pointer to the head of the list
+ * Return: sum of all data (n) or 0 if the list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
