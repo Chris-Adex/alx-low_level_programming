c5a65b02c2febf24fec329c01deb49c23a2b73ad
Oladipo Adediran <adediranchristopher98@gmail.com>
2024-08-09 04:27:30 +0100

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0 && index < 64)
 		return (0);
 
-	for (i = 0; x < 64; n >>= 1, x++)
+	for (x = 0; x < 64; n >>= 1, x++)
 	{
 		if (index == x)
 			return (n & 1);
